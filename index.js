4e4d766339f639cc7455597ca51e02bd9c593d56
overlookmotel <theoverlookmotel@gmail.com>
2021-06-20 11:54:38 +0100

    Deps: Update peer dependencies

--- index.js
+++ index.js
@@ -18,15 +18,14 @@ module.exports = {
 	],
 	rules: {
 		'jest/consistent-test-it': ['error', {fn: 'it', withinDescribe: 'it'}],
-		'jest/expect-expect': ['error'],
 		'jest/lowercase-name': ['error', {ignoreTopLevelDescribe: true}],
-		'jest/no-test-callback': ['error'],
+		'jest/no-alias-methods': ['error'],
+		'jest/no-disabled-tests': ['error'],
 		'jest/no-test-return-statement': ['error'],
 		'jest/prefer-to-be-null': ['error'],
 		'jest/prefer-to-be-undefined': ['error'],
 		'jest/prefer-to-contain': ['error'],
 		'jest/prefer-to-have-length': ['error'],
-		'jest/require-to-throw-message': ['error'],
-		'jest/no-disabled-tests': ['error']
+		'jest/require-to-throw-message': ['error']
 	}
 };
