4eaeb0bee6c3ef3c094d385e1465ce242d799d96
overlookmotel <theoverlookmotel@gmail.com>
2022-03-24 22:40:54 +0000

    Deps: Update to ESLint 8 [major]

--- index.js
+++ index.js
@@ -18,12 +18,11 @@ module.exports = {
 	],
 	rules: {
 		'jest/consistent-test-it': ['error', {fn: 'it', withinDescribe: 'it'}],
-		'jest/lowercase-name': ['error', {ignoreTopLevelDescribe: true}],
 		'jest/no-alias-methods': ['error'],
 		'jest/no-disabled-tests': ['error'],
 		'jest/no-test-return-statement': ['error'],
-		'jest/prefer-to-be-null': ['error'],
-		'jest/prefer-to-be-undefined': ['error'],
+		'jest/prefer-lowercase-title': ['error', {ignoreTopLevelDescribe: true}],
+		'jest/prefer-to-be': ['error'],
 		'jest/prefer-to-contain': ['error'],
 		'jest/prefer-to-have-length': ['error'],
 		'jest/require-to-throw-message': ['error']
--- test/index.test.js
+++ test/index.test.js
@@ -10,6 +10,6 @@ const eslintConfigJest = require('@overlookmotel/eslint-config-jest');
 
 // Tests
 
-it('Exports an object', () => { // eslint-disable-line jest/lowercase-name
+it('Exports an object', () => { // eslint-disable-line jest/prefer-lowercase-title
 	expect(eslintConfigJest).toBeObject();
 });
